3ad5b5031c32aa97e66160c6b7c32268b4155709
Noah Stinson <noahstjohn@ymail.com>
2020-10-04 17:54:37 -0500

    init commit

---
+++ model/Clock.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Timer;
+
+
+public abstract class Clock {
+
+	public int hours;
+	public int minutes;
+	public int seconds;
+
+	public long functionTime;
+	public long desiredTime;
+
+	Timer timer;
+	// updated when each clock started
+	protected long currentTime;
+	protected ActionListener listener;
+	
+	public Clock() {
+		setTimer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {}
+		});
+
+		System.out.println("Clock constructor called");
+	}
+
+	public Clock(int delay, ActionListener action) {
+		setTimer(delay, action);
+	}
+
+	protected void setTimer(int delay, ActionListener action) {
+		timer = new Timer(delay, action);
+	}
+
+	public void startTimer() {
+		updateCurrentTime();
+		desiredTime = currentTime + functionTime;
+		timer.start();
+	}
+
+	public void stopTimer() {
+		timer.stop();
+	}
+
+	public void updateCurrentTime() {
+		currentTime = System.currentTimeMillis();
+	}
+
+	protected abstract void defineListener();
+	public abstract void reset();
+	public abstract void setFunctionTime(int hours, int minutes, int seconds);
+	
+
+}
