9242e97d0f426171b6ac286ce874f3a813125530
Noah Stinson <noahstjohn@ymail.com>
2020-10-05 12:09:16 -0500

    main opens to timer

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import javax.swing.JFrame;
+
+import view.TimerScreen;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocation(200, 100);
+		window.setTitle("Clock");
+
+		TimerScreen timerScreen = new TimerScreen(window);
+		timerScreen.init();
+
+		//window.pack();
+		window.setVisible(true);
+
 	}
 }
