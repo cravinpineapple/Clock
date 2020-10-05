9999c750ebbf2c83208e6e952f5b6d6b04e1641b
Noah Stinson <noahstjohn@ymail.com>
2020-10-05 12:10:03 -0500

    added render/set functionTime additions

--- model/Timer.java
+++ model/Timer.java
@@ -1,9 +1,18 @@
 package model;
 
 import java.awt.event.ActionListener;
+
+import view.TimerCanvas;
+
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Font;
 
 public class Timer extends Clock {
+
+	// canvas needed to repaint each cycle
+	TimerCanvas canvas;
 
 	public Timer() {
 		super();
@@ -20,6 +29,7 @@ public class Timer extends Clock {
 				currentTime += 1000;
 				updateHoursMinutesSeconds();
 				System.out.println(hours + ":" + minutes + ":" + seconds);
+				canvas.repaint();
 
 				if (currentTime >= desiredTime)
 					stopTimer();
@@ -40,7 +50,39 @@ public class Timer extends Clock {
 
 	@Override
 	public void setFunctionTime(int hours, int minutes, int seconds) {
+		this.hours = hours;
+		this.minutes = minutes;
+		this.seconds = seconds;
+
 		functionTime = (hours * 3600000) + (minutes * 60000) + (seconds * 1000);
+	}
+
+	@Override
+	public void render(Graphics2D g2) {
+		g2.setColor(Color.gray);
+		g2.setFont(new Font("Courier", Font.BOLD, 100));
+		if (hours > 9)
+			g2.drawString("" + hours, 40, 115);
+		else 
+			g2.drawString("0" + hours, 40, 115);
+
+		g2.drawString(":", 160, 115);
+
+		if (minutes > 9)
+			g2.drawString("" + minutes, 200, 115);
+		else
+			g2.drawString("0" + minutes, 200, 115);
+
+		g2.drawString(":", 320, 115);
+
+		if (seconds > 9)
+			g2.drawString("" + seconds, 360, 115);
+		else
+			g2.drawString("0" + seconds, 360, 115);
+	}
+
+	public void setCanvas(TimerCanvas canvas) {
+		this.canvas = canvas;
 	}
 	
 }
