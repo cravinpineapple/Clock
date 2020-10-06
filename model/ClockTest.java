b650a52b3ed43fcc9ad176cb5f4d7cafb1f72c1b
Noah Stinson <noahstjohn@ymail.com>
2020-10-06 01:24:55 -0500

    test code

--- model/ClockTest.java
+++ model/ClockTest.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 public class ClockTest {
 	public static void main(String args[]) {
+		/*
 		Clock timer = new Timer();
 		timer.updateCurrentTime();
 		System.out.println("Current Time: " + timer.currentTime);
@@ -11,9 +12,25 @@ public class ClockTest {
 		System.out.println("Timer Time: " + timer.functionTime);
 
 		timer.startTimer();
+		*/
+		
+		Alarm alarm = new Alarm();
+		alarm.updateCurrentTime();
+		System.out.println("Current Time: " + alarm.currentTime);
+		System.out.println("Alarm Time Format: " + alarm.alarmString);
+		System.out.println(alarm.toString());
+		alarm.startTimer();
 
+		
+		/*
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.startTimer();
+		*/
+		
 		JFrame window = new JFrame();
 		window.setVisible(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 	}
 }
