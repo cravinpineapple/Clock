5121f0218ccd9c9b48c6349975d4a1c73e833734
Noah Stinson <noahstjohn@ymail.com>
2020-10-06 14:24:47 -0500

    cleaned code

--- model/Alarm.java
+++ model/Alarm.java
@@ -7,6 +7,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import view.AlarmScreen;
 
 import java.awt.event.ActionEvent;
@@ -21,6 +25,7 @@ public class Alarm extends Clock {
 
 	public DateFormat format = new SimpleDateFormat("HH:mm:ss");
 	Date date;
+	JFrame window;
 
 	public Alarm() {
 		super();
@@ -29,20 +34,21 @@ public class Alarm extends Clock {
 		//buildAlarmString(); for testing
 	}
 
-	public void buildAlarmString() {
-		int hoursIndex = panel.getHoursCombo().getSelectedIndex();
-		int minutesIndex = panel.getMinutesCombo().getSelectedIndex();
-		int secondsIndex = panel.getSecondsCombo().getSelectedIndex();
+	public void buildAlarmTime(JComboBox<String> hoursCombo, JComboBox<String> minutesCombo, JComboBox<String> secondsCombo) {
+		hours = hoursCombo.getSelectedIndex();
+		minutes = minutesCombo.getSelectedIndex();
+		seconds = secondsCombo.getSelectedIndex();
 
-		String hoursString = hoursIndex < 10 ? "0" + hoursIndex : Integer.toString(hoursIndex);
-		String minutesString = minutesIndex < 10 ? "0" + minutesIndex : Integer.toString(minutesIndex);
-		String secondsString = secondsIndex < 10 ? "0" + secondsIndex : Integer.toString(secondsIndex);
+		String hoursString = hours < 10 ? "0" + hours : Integer.toString(hours);
+		String minutesString = minutes < 10 ? "0" + minutes : Integer.toString(minutes);
+		String secondsString = seconds < 10 ? "0" + seconds : Integer.toString(seconds);
 
 		alarmString = hoursString + ":" + minutesString + ":" + secondsString;
 	}
 
-	public void setAlarmScreen(AlarmScreen panel) {
+	public void setAlarmScreenAndWindow(AlarmScreen panel) {
 		this.panel = panel;
+		this.window = panel.getWindow();
 	}
 
 	@Override
@@ -51,9 +57,10 @@ public class Alarm extends Clock {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				currentTime += 1000;
-
 				if (externalToString().equals(alarmString)) {
-					timer.stop();
+					stopTimer();
+					reset();
+					JOptionPane.showMessageDialog(window, "Alarm Time Reached!");
 				}
 			}
 		};
@@ -71,21 +78,22 @@ public class Alarm extends Clock {
 
 	@Override
 	public void reset() {
-		hours = 0;
-		minutes = 0;
-		seconds = 0;
+		JComboBox<String> hoursCombo = panel.getHoursCombo();
+		JComboBox<String> minutesCombo = panel.getMinutesCombo();
+		JComboBox<String> secondsCombo = panel.getSecondsCombo();
+		buildAlarmTime(hoursCombo, minutesCombo, secondsCombo);
+		panel.getOffButton().setSelected(true);
+		hoursCombo.setSelectedIndex(0);
+		minutesCombo.setSelectedIndex(0);
+		secondsCombo.setSelectedIndex(0);
+		hoursCombo.setEnabled(true);
+		minutesCombo.setEnabled(true);
+		secondsCombo.setEnabled(true);
 	}
 
 	@Override
 	public void setFunctionTime(int hours, int minutes, int seconds) {
-		if (hours == 12)
-			hours = 0;
 
-		this.hours = hours;
-		this.minutes = minutes;
-		this.seconds = seconds;
-
-		functionTime = (hours * 3600000) + (minutes * 60000) + (seconds * 1000);
 	}
 
 	@Override
